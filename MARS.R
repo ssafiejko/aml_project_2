c1ab60edbe8cf45a9a2a31b7b6ac0108814ef4ee
Wojtek Grabias <wojtekgrabias@Mac-mini-Wojtek.local>
2025-06-02 15:18:04 +0200

    features

--- MARS.R
+++ MARS.R
@@ -8,10 +8,10 @@ colnames(df) <- paste0("X", 1:ncol(X))
 df$target <- y
 
 # cols_to_remove <- c()
-# cols_to_remove <- c(13, 178, 194, 298, 305, 117, 228, 462, 414, 425, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9)+1 # VIF-removed variables
-df <- df[, c(3)]
+cols_to_remove <- c(13, 178, 194, 298, 305, 117, 228, 462, 414, 425, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9)+1 # VIF-removed variables
+df <- df[, -cols_to_remove]
 str(df)
 
-mars_model <- earth(target ~ ., data = df, degree = 1) # degree = 2
+mars_model <- earth(target ~ ., data = df, degree = 2) # degree = 2
 ev <- evimp(mars_model)
 print(ev)
